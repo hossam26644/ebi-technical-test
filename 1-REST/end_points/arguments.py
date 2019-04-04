de3468a001fd958e1786b21b9ec20fa6c07f2aa8
Hossam <hossam26644@live.com>
2019-04-04 14:54:07 +0100

    help for gene_suggest end point arguments

--- 1-REST/end_points/arguments.py
+++ 1-REST/end_points/arguments.py
@@ -2,6 +2,16 @@
 from flask_restplus import reqparse
 
 gene_suggestion_arguments = reqparse.RequestParser()
-gene_suggestion_arguments.add_argument('query', type=str, help='Query', default="")
-gene_suggestion_arguments.add_argument('species', type=str, help='Species', required=True)
-gene_suggestion_arguments.add_argument('limit', type=int, help='Limit', required=True)
+gene_suggestion_arguments.add_argument('query',
+									   type=str,
+									   help='the partial query typed by the user, e.g. `brc` ',
+									   default="")
+
+gene_suggestion_arguments.add_argument('species',
+									    type=str, help='the name of the target species, e.g. `homo_sapiens`',
+									    required=True)
+
+gene_suggestion_arguments.add_argument('limit',
+										type=int,
+										help='the maximum number of suggestions to return, e.g. `10`',
+										required=True)
