d3fba2246e33278860c18786a91b78ae529e375a
Hossam <hossam26644@live.com>
2019-04-04 10:45:32 +0100

    end point separated from main

---
+++ 1-REST/end_points/gene_operations.py
@@ -0,0 +1,47 @@
+''' Module '''
+''' Ge'''
+from flask_restplus import Resource
+from apis.api import api
+from models import models
+
+
+from flask_restplus import reqparse
+
+arguments = reqparse.RequestParser()
+arguments.add_argument('query', type=str, help='Query', default="")
+arguments.add_argument('species', type=str, help='Species', required=True)
+arguments.add_argument('limit', type=int, help='Limit', required=True)
+arguments = api.namespace('gene-operations',
+                                          description='EBI technical test service')
+
+@gene_operations_namespace.route('/gene-suggest')
+class GeneSuggest(Resource):
+    ''' bla bla'''
+
+    @api.expect(arguments)
+    def get(self):
+        ''' bla bla'''
+        args = arguments.parse_args()
+        query = args.get('query')
+        species = args.get('species')
+        limit = args.get('limit')
+
+        #retrive_list_from_database function
+        gene_suggestions = self.retrive_list_from_database(query, species, limit)
+        #Using marshmallow for serialization (using the Geneschema)
+        gene_schema = models.GeneSchema(many=True)
+        output = gene_schema.dump(gene_suggestions).data
+        #Return results in JSON to the user
+        return {'result_list': output}
+
+    #Takes query arguments and uses ORM model to get results
+    def retrive_list_from_database(self, query, species, limit):
+        ''' bla bla'''
+        gene = models.Gene
+        gene_suggestions = gene.query.filter_by(species=species)	\
+                                    .filter(gene.display_label.like(query+'%'))\
+                                    .with_entities(gene.display_label)\
+                                    .group_by(gene.display_label)  \
+                                    .order_by(gene.display_label)\
+                                    .limit(limit).all()
+        return gene_suggestions
--- 1-REST/main.py
+++ 1-REST/main.py
@@ -1,66 +1,20 @@
-from flask import Flask, request
-from flask_restplus import Resource, Api, reqparse, fields
+''' Main file, run to start service '''
+from logging import FileHandler, WARNING
 
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
+from apis.api import api
+from configurations import DeploymentConfigurations as configuration
 
-
+#main elements
 app = Flask(__name__) #the main app object
 db = SQLAlchemy(app) #the database object
 ma = Marshmallow(app) #marshmallow serialization object
+FILE_HANDLER = FileHandler('errorlogs.txt') #file handler for logging
+FILE_HANDLER.setLevel(WARNING)
 
-app = Flask(__name__)                  #  Create a Flask WSGI application
-app.config['RESTPLUS_MASK_SWAGGER'] = False
-api = Api(app, doc='/documentation/')                       #  Create a Flask-RESTPlus API
-#app.register_blueprint(api, url_prefix='/api/1')
-db = SQLAlchemy(app)
-#ma = Marshmallow(app)
-
-
-engine = db.create_engine("mysql://anonymous@ensembldb.ensembl.org:3306/ensembl_website_90",echo = True)
-metadata = db.MetaData()
-census = db.Table('gene_autocomplete', metadata, autoload=True, autoload_with=engine)
-
-ns_conf = api.namespace('bla', description='Conference operations')
-
-
-@ns_conf.route('/gene_suggest') 
-@api.doc(params={'id': 'An ID'})       #  Create a URL route to this resource
-class HelloWorld(Resource):            #  Create a RESTful resource
-    def get(self):                     #  Create GET endpoint
-        return {'hello': 'HossamWorld'}
-
-pagination = reqparse.RequestParser()
-pagination.add_argument('query', type=str, help='Query',default="")
-pagination.add_argument('species', type=str, help='Species',required=True)
-pagination.add_argument('limit', type=int, help='Limit',required=True)
-
-ResponceBodyModel = api.model('suggestions',{'SuggestedGeneNames':fields.String})
-
-@ns_conf.route('/') 
-class Connection(Resource):            #  Create a RESTful resource
-	@api.expect(pagination)
-	@api.marshal_with(ResponceBodyModel)
-	def get(self):
-		args = pagination.parse_args()
-		query = args.get('query')
-		species = args.get('species')
-		limit = args.get('limit')
-		
-
-		sqlquery = db.select([census.c.stable_id]).where(db.and_(census.c.stable_id.like('ENSAMEG%'),
-															  census.c.species == 'ailuropoda_melanoleuca')).limit(10).order_by(census.c.stable_id)
-		connection = engine.connect()
-		ResultProxy = connection.execute(sqlquery)
-		ResultSet = ResultProxy.fetchall()
-		print("************")
-		print((ResultSet))
-		output = []
-		
-		for x in ResultSet:
-		  output.append(x[0]) 
-
-		return {'SuggestedGeneNames':output}
 
 def configure_app(app, configuration):
     ''' config app from confiuration file '''
@@ -71,15 +25,16 @@ def configure_app(app, configuration):
     app.config['TESTING'] = configuration.FLASK_TESTING
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = configuration.SQLALCHEMY_TRACK_MODIFICATIONS
 
-def initialize_app(app):
+def initialize_app(app, configuration):
     ''' initialize app object'''
+    #get name space, TODO// relocate to import list
+    from end_points.gene_operations import gene_operations_namespace
+    app.logger.addHandler(FILE_HANDLER)
     configure_app(app, configuration)
     api.init_app(app)
-    api.add_namespace(ns_conf)
-				
-		
-
+    api.add_namespace(gene_operations_namespace)
 
 if __name__ == '__main__':
-	initialize_app(app)
-    app.run(debug=True) 
+    initialize_app(app, configuration)
+    app.run(debug=configuration.FLASK_DEBUG)
+
