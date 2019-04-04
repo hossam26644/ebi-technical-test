f8d8bab92b1ffeb07c6f3ce627170596a1b8d173
Hossam <hossam26644@live.com>
2019-04-04 09:37:54 +0100

    documentation end point changed

---
+++ 1-REST/api/api.py
@@ -0,0 +1,12 @@
+''' Api module '''
+from flask_restplus import Api
+
+#create api
+api = Api(doc='/documentation/', title='gene_suggest service', version='1.0',
+          description='Responds with a list of suggested gene names for the given query and target species.')
+
+@api.errorhandler
+def internal_server_erroe_exception(error):
+    ''' internal server error exeption '''
+    message = 'An unhandled exception occurred.'
+    return {'message': message}, 500
